980b4fa9c6fc709741342a5f181a9516766bbc19
ErnestoRe <ernestorezustaaznarez@gmail.com>
2024-11-19 11:46:05 +0100

    commit funciones1

--- Reto 4/src/reto4/funciones1.java
+++ Reto 4/src/reto4/funciones1.java
@@ -1,5 +1,15 @@
 package reto4;
 
 public class funciones1 {
- 
+	public static void dimelo(int producto, int num) {// hace una piramide
+		for (producto = 4, num = 1; producto >= 0; producto--, num += 2) {
+			System.out.println();
+			for (int i = 0; i <= producto; i++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= num; j--) {
+				System.out.print("*");
+			}
+		}
+	}
 }
