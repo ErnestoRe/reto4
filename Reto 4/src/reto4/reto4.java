1ec4bd3e4ff9089054cf353b67e62c06fd6f9ac8
dPiazuelo <DAM1@aula11-pc27>
2024-11-19 11:52:01 +0100

    Merge branch 'main' of https://github.com/ErnestoRe/reto4.git into main commit final

--- Reto 4/src/reto4/reto4.java
+++ Reto 4/src/reto4/reto4.java
@@ -1,6 +1,30 @@
 package reto4;
 
+import java.util.Scanner;
+
 public class reto4 {
+	// este main calcula los multiplos de un número y luego con
+	// la funcion hace una piramide con es numero de estrellas
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		int i = 0;
+		int producto = 0;
+		//Aqui pide un número 
+		System.out.println("Introduce un número");
+		funciones.dimeEntero(null, sc);
+		do {
+			for (i = num; i <= 0; i--)
+				;
+			{
+
+				producto = num * i;
+			}
+
+		} while (producto != 0);
+		funciones1.dimelo(producto, num);
+	}
+	
 
 }
- 
